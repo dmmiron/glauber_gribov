c3afee450c9c7810cdf4988a21205cece47223ae
Daniel Miron <you@example.com>
2015-01-19 21:19:44 -0500

    cleaned up. nucleus class working. need to start collision class

--- dijet/density.cxx
+++ dijet/density.cxx
@@ -1,40 +1,6 @@
-#include <TMath.h>
-#include <TF1.h>
-#include <TF2.h>
-#include <TFile.h>
-#include <TString.h>
-#include <limits>
+#include "density.h"
 
 using namespace std;
-
-class Nucleus 
-{
-    private:
-        Double_t    fRho0;  //woods saxon rho nought parameter
-        Double_t    fR0;    //"mean" radius
-        Double_t    fMu;    //woods-saxon scaling parameter
-        TF1*        fDensity; //nucleon density function
-        TF1*        fThickness; //thickness as function of distance from center (impact parameter)
-        TF1*        fThickIntegrand; //Integrand used for thickness funciton
-        Double_t    fMaxR;      //maximum r used for calculating integrals
-        void        Update();  // sets fDensity and fThickness
-        TF1*        MakeThicknessIntegrand();
-        TF1*        MakeThicknessFunc();
-    public:
-       Nucleus(Double_t iRho0=1, Double_t iR0=1, Double_t iMu=1);
-       
-       Double_t     GetRho0()                 const {return fRho0;}
-       Double_t     GetR0()                   const {return fR0;}
-       Double_t     GetMu()                   const {return fMu;}
-       Double_t     GetDensity(Double_t r)    const {return fDensity->Eval(r);}
-       Double_t     GetThickness(Double_t b)  const {return fThickness->Eval(b);}
-       TF1*         GetDensityFunc()          const {return fDensity;}
-       TF1*         GetThicknessFunc()        const {return fThickness;}
-       void         SetRho0(Double_t iRho0);
-       void         SetR0(Double_t iR0);
-       void         SetMu(Double_t iMu);
-       Double_t     Thickness(Double_t *b, Double_t *);
-};
 
 Nucleus::Nucleus(Double_t iRho0, Double_t iR0, Double_t iMu) {
     fRho0 = iRho0;
@@ -54,40 +20,6 @@ void Nucleus::Update() {
 
     fThickIntegrand = MakeThicknessIntegrand();
     fThickness = MakeThicknessFunc();
-}
-
-//function object (functor) 
-struct MyDerivFunc { 
-    MyDerivFunc(TF1 *f): fFunc(f) {}
-    double operator() (double *x, double * )  const { 
-        return fFunc->Derivative(*x);
-    }
-    TF1 * fFunc; 
-};
-
-struct MyIntegFunc {
-    MyIntegFunc(TF1 *f): fFunc(f) {}
-    double operator() (double *x, double *par) const {
-        double a = fFunc->GetXmin();
-        return fFunc->Integral(a, *x);
-    }
-    TF1 *fFunc;
-};
-
-struct ThicknessFunc {
-    ThicknessFunc(TF1 *f): fFunc(f) {}
-    double operator() (double *x, double *par) const{
-        double b = *x;
-        fFunc->SetParameter(0, b);
-        return fFunc->Integral(b, 10);
-    }
-    TF1 *fFunc;
-};
-
-Double_t Nucleus::Thickness(Double_t *b, Double_t *) {
-    Double_t bb = *b;
-    fThickIntegrand->SetParameter(0, bb);
-    return fThickIntegrand->Integral(bb, fMaxR);
 }
 
 TF1* Nucleus::MakeThicknessFunc(){
--- dijet/density.h
+++ dijet/density.h
@@ -1,1 +1,74 @@
+#ifndef DENSITY_CXX
+#define DENSITY_CXX
 
+class Nucleus;
+
+#include <TMath.h>
+#include <TF1.h>
+#include <TF2.h>
+#include <TFile.h>
+#include <TString.h>
+#include <limits>
+
+
+
+class Nucleus 
+{
+    private:
+        Double_t    fRho0;  //woods saxon rho nought parameter
+        Double_t    fR0;    //"mean" radius
+        Double_t    fMu;    //woods-saxon scaling parameter
+        TF1*        fDensity; //nucleon density function
+        TF1*        fThickness; //thickness as function of distance from center (impact parameter)
+        TF1*        fThickIntegrand; //Integrand used for thickness funciton
+        Double_t    fMaxR;      //maximum r used for calculating integrals
+        void        Update();  // sets fDensity and fThickness
+        TF1*        MakeThicknessIntegrand();
+        TF1*        MakeThicknessFunc();
+    public:
+        Nucleus(Double_t iRho0=1, Double_t iR0=1, Double_t iMu=1);
+        
+        Double_t     GetRho0()                 const {return fRho0;}
+        Double_t     GetR0()                   const {return fR0;}
+        Double_t     GetMu()                   const {return fMu;}
+        Double_t     GetDensity(Double_t r)    const {return fDensity->Eval(r);}
+        Double_t     GetThickness(Double_t b)  const {return fThickness->Eval(b);}
+        TF1*         GetDensityFunc()          const {return fDensity;}
+        TF1*         GetThicknessFunc()        const {return fThickness;}
+        void         SetRho0(Double_t iRho0);
+        void         SetR0(Double_t iR0);
+        void         SetMu(Double_t iMu);
+};
+
+//Functors for calculating derivatives and integrals of TF1 functions.
+struct MyDerivFunc { 
+    MyDerivFunc(TF1 *f): fFunc(f) {}
+    Double_t operator() (Double_t *x, Double_t * )  const { 
+        return fFunc->Derivative(*x);
+    }
+    TF1 * fFunc; 
+};
+
+struct MyIntegFunc {
+    MyIntegFunc(TF1 *f): fFunc(f) {}
+    Double_t operator() (Double_t *x, Double_t *par) const {
+        Double_t a = fFunc->GetXmin();
+        return fFunc->Integral(a, *x);
+    }
+    TF1 *fFunc;
+};
+
+//Functor for calculating Thickness as a function of b (impact parameter)
+// when integrand is a function of r and needs to be integrated wrt z
+struct ThicknessFunc {
+    ThicknessFunc(TF1 *f): fFunc(f) {}
+    Double_t operator() (Double_t *x, Double_t *par) const{
+        Double_t b = *x;
+        fFunc->SetParameter(0, b);
+        return fFunc->Integral(b, 10);
+    }
+    TF1 *fFunc;
+};
+
+
+#endif
